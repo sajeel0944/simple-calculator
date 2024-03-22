6ad1ddcb0dfc5e2d4cfc6e8c51013c9d322f682b
sajeek0944 <sajeel6y4z@gmail.com>
2024-03-23 01:16:10 +0500

    calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number ", type: "number", name: "FirstNumber" },
+    { message: "Enter second number", type: "number", name: "SecondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition",
+            "Substraction",
+            "Multiplication",
+            "Division",
+            "Exponentiation",
+            "Modulus",
+        ],
+    },
+]);
+//condition statement
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else if (answer.operator === "Exponentiation") {
+    console.log(answer.FirstNumber ** answer.SecondNumber);
+}
+else if (answer.operator === "Modulus") {
+    console.log(answer.FirstNumber % answer.SecondNumber);
+}
+else {
+    console.log("Error");
+}
